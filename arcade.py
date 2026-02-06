83319d08010e6938ce28efd3e997241de3519258
kisukk <v.danis2009@gmail.com>
2026-02-06 18:38:21 +0300

    начало

---
+++ arcade.py
@@ -0,0 +1,13 @@
+import arcade
+import random
+
+
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+SCREEN_TITLE = "Пешерный пацик"
+PLAYER_SPEED = 5
+
+
+class Player:
+    def __init__(self):
+        super().__init__()
