5347b8bc5cb6d1d70314af1d465302ceff4fcf52
kisukk <v.danis2009@gmail.com>
2026-02-06 19:34:21 +0300

    картинкидлявсех

--- arcade.py
+++ arcade.py
@@ -10,4 +10,24 @@ PLAYER_SPEED = 5
 
 class Player(arcade.Sprite):
     def __init__(self):
-        super().__init__("PICTURES/.png")
+        super().__init__("PICTURES/pacik/idle_viking.png")
+
+
+class Enemy(arcade.Sprite):
+    def __init__(self):
+
+
+class Platform():
+    def __init__(self):
+
+class Coins():
+    def __init__(self):
+
+class Door():
+    def __init__(self):
+
+class Shop():
+    def __init__(self):
+
+class Game():
+    def __init__(self):
